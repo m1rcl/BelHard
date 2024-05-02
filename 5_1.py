bd2091e48bec7407ab6d28c6cf8badd001101ea8
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-05-02 16:57:36 +0300

    final hw 5_1

--- 5_1.py
+++ 5_1.py
@@ -6,18 +6,17 @@ try:
     count_numbers = int(input("Введите количество выводимых чисел: "))
     if count_numbers <= 0:
         raise ValueError
-    multiplicity_number = int(
-        input("Введите целое число которому должно быть кратны выводимые числа: "))
+    multiplicity_number = abs(int(
+        input("Введите целое число отличное от ноля, которому должно быть кратны выводимые числа: ")))
     if multiplicity_number == 0:
         raise ValueError
 
     number += 1
     if (number % multiplicity_number):
-        number = (number // abs(multiplicity_number) + 1) * \
-            abs(multiplicity_number)
+        number = (number // multiplicity_number + 1) * multiplicity_number
     for _ in range(count_numbers):
         print(number, end=" ")
-        number = number + abs(multiplicity_number)
+        number = number + multiplicity_number
 
 except ValueError:
     print("Введены неверные данные")
