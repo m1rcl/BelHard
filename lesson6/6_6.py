34e65315a92ec0987ae51cc7275b5e9215b89576
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-05-07 23:13:43 +0300

    fix names of lists

--- lesson6/6_6.py
+++ lesson6/6_6.py
@@ -6,10 +6,10 @@ def filtering_list(some_list):
     even_list = []
     for _ in some_list:
         if _ % 2:
-            even_list.append(_)
-        else:
             odd_list.append(_)
-    return odd_list + even_list
+        else:
+            even_list.append(_)
+    return even_list + odd_list
 
 
 some_list = [4, 7, 34, 67, 23, 0, 5]
