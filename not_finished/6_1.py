1802e3658f11664985a39fc3ea13f01807246851
Konstantin Kiselev <kiselev-kostya@tut.by>
2024-05-07 17:28:00 +0300

    6_1 6_7 not finished

--- not_finished/6_1.py
+++ not_finished/6_1.py
@@ -1,13 +1,14 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без
 # использования функции int
 
-def convert_str_to_int(str_number):
+def convert_str(str_number):
     number = 0
     dict_number = {'0': 0, '1': 1, '2': 2, '3': 3,
                    '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
     for _ in range(0, len(str_number)):
         number = number + \
             (dict_number[str_number[len(str_number) - 1 - _]] * (10**(_)))
+    return number
 
 
 def dec_to_bin(number):
@@ -20,13 +21,15 @@ def dec_to_bin(number):
 
 def bin_to_dec(number):
     result = 0
-    for _ in range(0, len(number)):
-        result = result +
+    for _ in range(0, len(str(number))):
+        result = result + (number // 10**(len(str(number)) - 1 - n)
+                           ) * (2**(len(str(number)) - 1))
+    return result
 
 
 user_number = input(
     "Введите число в десятичной системе, для перевода в двоичную систему и обратно: ")
-dec_number = convert_str_to_int(user_number)
-print(f"Число {dec_number} в двойчном счислении: {dec_to_bin(dec_number)}")
-bin_number = convert_str_to_int(dec_number)
-print(f"Число {dec_number} в десятичном счислении: {bin_to_dec(bin_number)}")
+dec_number = convert_str(user_number)
+print(f"Число {dec_number} в двойчном исчислении: {dec_to_bin(dec_number)}")
+bin_number = convert_str(dec_to_bin(dec_number))
+print(f"Число {bin_number} в десятичном исчислении: {bin_to_dec(bin_number)}")
